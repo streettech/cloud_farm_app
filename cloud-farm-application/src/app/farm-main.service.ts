3494dc290ddfd3d242016d785906a796f5cffb31
streettech <muketefrank@gmailcom>
2022-02-23 21:43:58 -0600

    added to css and services farm-main and styles

--- cloud-farm-application/src/app/farm-main.service.ts
+++ cloud-farm-application/src/app/farm-main.service.ts
@@ -13,8 +13,7 @@ export class FarmMainService {
     const farm_info = [
       {"id": 6742411, "name": "Farm1", "type": "simple"},
       {"id": 4745145, "name": "Farm2", "type": "intermidiate"},
-      {"id": 1475534, "name": "Farm3", "type": "advanced"},
-      {"id": 4755316, "name": "Farm4", "type": "intermidiate"}
+      {"id": 1121541, "name": "Farm3", "type": "advanced"}
     ];
     return farm_info;
   }
