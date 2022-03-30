3a26f7462952184dd7eac500bfcd742ec02c0939
streettech <muketefrank@gmailcom>
2022-03-30 12:43:53 -0500

    modified system architecture. Added frontend and backend folders and preparing for microprocesses switch. branching v2 local for new development. setting path to better version control

--- cloud-farm-application/src/app/app-routing.module.ts
+++ cloud-farm-application/src/app/app-routing.module.ts
@@ -1,30 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { FarmComponent } from './farm/farm.component';
-import { FarmsIndiComponent } from './farms-indi/farms-indi.component';
 import { HomeComponent } from './home/home.component';
 import { HubComponent } from './hub/hub.component';
-import { LabComponent } from './lab/lab.component';
-import { NewfarmComponent } from './newfarm/newfarm.component';
-import { SpecComponent } from './spec/spec.component';
-import { DocumentationComponent } from './documentation/documentation.component';
-import { MessagingComponent } from './messaging/messaging.component';
-import { FarmIndiComponent } from './farm-indi/farm-indi.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'hub', component: HubComponent},
-  {path: 'farm', component: FarmComponent},
-  {path: 'newfarm', component: NewfarmComponent},
-  {path: 'lab', component: LabComponent},
-  {path: 'spec', component: SpecComponent},
-  {path: 'farms_indi', component: FarmsIndiComponent},
-  {path: 'documentation', component: DocumentationComponent},
-  {path: 'spec', component: SpecComponent},
-  {path: 'messaging', component: MessagingComponent},
-  {path: 'farm-indi', component: FarmIndiComponent}
 ];
 
 @NgModule({
@@ -34,7 +17,4 @@ const routes: Routes = [
 export class AppRoutingModule { }
 export const routingComponents = [
   HomeComponent, AboutComponent, HubComponent, 
-  FarmComponent, NewfarmComponent, LabComponent, 
-  SpecComponent, FarmsIndiComponent, DocumentationComponent,
-  MessagingComponent, FarmIndiComponent
 ]
--- cloud-farm-application/src/app/app.module.ts
+++ cloud-farm-application/src/app/app.module.ts
@@ -5,38 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HubComponent } from './hub/hub.component';
-import { FarmComponent } from './farm/farm.component';
-import { NewfarmComponent } from './newfarm/newfarm.component';
-import { LabComponent } from './lab/lab.component';
-import { SpecComponent } from './spec/spec.component';
-import { FarmsIndiComponent } from './farms-indi/farms-indi.component';
 import { FarmMainService } from './farm-main.service';
-import { DocumentationComponent } from './documentation/documentation.component';
-import { DummyComponent } from './dummy/dummy.component';
-import { MessagingComponent } from './messaging/messaging.component';
-import { FarmIndiComponent } from './farm-indi/farm-indi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    TestComponent,
     HeaderComponent,
     FooterComponent,
-    HubComponent,
-    FarmComponent,
-    NewfarmComponent,
-    LabComponent,
-    SpecComponent,
-    FarmsIndiComponent,
-    DocumentationComponent,
-    DummyComponent,
-    MessagingComponent,
-    FarmIndiComponent,
+    HubComponent
   ],
   imports: [
     BrowserModule,
